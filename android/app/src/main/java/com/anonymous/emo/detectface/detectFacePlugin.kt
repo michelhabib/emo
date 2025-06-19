145c9dfa5d0182ede123c56b4600220f0e885087
Michel Habib <michelhabib@gmail.com>
2025-06-19 18:44:37 +0300

    feat: implement face detection functionality in detectFacePlugin; update package.json and package-lock.json for babel-preset-expo

--- android/app/src/main/java/com/anonymous/emo/detectface/detectFacePlugin.kt
+++ android/app/src/main/java/com/anonymous/emo/detectface/detectFacePlugin.kt
@@ -1,12 +1,45 @@
 package com.anonymous.emo.detectface
 
+import android.annotation.SuppressLint
+import com.google.android.gms.tasks.Tasks
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 import com.mrousavy.camera.frameprocessors.Frame
 import com.mrousavy.camera.frameprocessors.FrameProcessorPlugin
 import com.mrousavy.camera.frameprocessors.VisionCameraProxy
 
 class detectFacePlugin(proxy: VisionCameraProxy, options: Map<String, Any>?): FrameProcessorPlugin() {
+  
+  private val detector = FaceDetection.getClient(
+    FaceDetectorOptions.Builder()
+      .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+      .build()
+  )
+
+  @SuppressLint("UnsafeOptInUsageError")
   override fun callback(frame: Frame, arguments: Map<String, Any>?): Any? {
-    // code goes here
-    return null
+    val image = InputImage.fromMediaImage(frame.image, frame.imageInfo.rotationDegrees)
+    
+    return try {
+      val faces = Tasks.await(detector.process(image))
+      faces.map { face -> faceToJson(face) }
+    } catch (e: Exception) {
+      e.printStackTrace()
+      emptyList<Map<String, Any>>()
+    }
+  }
+  
+  private fun faceToJson(face: Face): Map<String, Any> {
+    return mapOf(
+      "boundingBox" to mapOf(
+        "left" to face.boundingBox.left,
+        "top" to face.boundingBox.top,
+        "right" to face.boundingBox.right,
+        "bottom" to face.boundingBox.bottom
+      ),
+      "trackingId" to (face.trackingId ?: -1)
+    )
   }
 }
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
-    'expo-router/babel',
     'react-native-worklets-core/plugin',
     'react-native-reanimated/plugin',   // Reanimated must stay last
   ],
